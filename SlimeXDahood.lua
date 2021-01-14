5c2a81bb2fe6d6f93314d0d349064b475ab9d023
SlimePlayz <76547101+SlimePlayz@users.noreply.github.com>
2021-01-14 12:35:35 -0800

    Add files via upload

---
+++ SlimeXDahood.lua
@@ -0,0 +1,5 @@
+-- © SlimeX 2021 - 2022
+
+-- Developed By Slime#0954 | https://github.com/SlimePlayz/SlimeX-Hub.git
+-- Discord: https://discord.gg/YkmQeSUSZA
+-- Thank you for using SlimeX Da Hood script | In Works
