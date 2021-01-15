1d1b86e053a83e8e96087f5fdecae1e59c0ea0a9
SlimePlayz <76547101+SlimePlayz@users.noreply.github.com>
2021-01-15 12:46:40 -0800

    Update and rename SlimeXBaseRaiders.lua to SlimeXCaliberGun.lua

--- SlimeXBaseRaiders.lua
+++
@@ -1,5 +0,0 @@
--- © SlimeX 2021 - 2022
-
--- Developed By Slime#0954 | https://github.com/SlimePlayz/SlimeX-Hub.git
--- Discord: https://discord.gg/YkmQeSUSZA
--- Thank you for using SlimeX Base Raiders script | In Works
